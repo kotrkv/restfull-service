cff654dbb961f832176720e7fe7fd84b4a4c1395
konstantin <konstantin@MacBook-Pro-konstantin.local>
2024-01-06 21:50:33 +0300

    Add server

---
+++ rServer.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// type User struct {
+// 	Username string `json:"user"`
+// 	Password string `json:"password"`
+// }
+
+// var User user
+var PORT = ":1234"
+var DATA = make(map[string]string)
+
+func main() {
+	fmt.Println("Hello Restfull server....")
+	arguments := os.Args
+	if len(arguments) != 1 {
+		PORT = ":" + arguments[1]
+	}
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+	mux.Handle("/", http.HandlerFunc(defaultHandler))
+	fmt.Println("Ready to serve at", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusNotFound)
+	Body := "Thanks for visiting!\n"
+	fmt.Fprintf(w, "%s", Body)
+}
